2c4d3b318c6c1941ee5e5d778db669bfc89b17bb
Angelo N. L <ledonneangelo02@gmail.com>
2023-09-20 11:19:51 -0500

    Function for login popup working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  Showlogin(): void{
+    const forDisplay = document.getElementById("LoginForm");
+    const logButton = document.getElementById("OpenLogin");
+    const closeButton = document.getElementById("CloseLogin");
+    if(forDisplay != null && logButton != null && closeButton != null){
+      forDisplay.style.display = "block";
+      logButton.style.display = "none";
+      closeButton.style.display="block";
+    }
+  }
+
+  CloseLogin(): void{
+    const forDisplay = document.getElementById("LoginForm");
+    const logButton = document.getElementById("OpenLogin");
+    const closeButton = document.getElementById("CloseLogin");
+    if(forDisplay != null && logButton != null && closeButton != null){
+      forDisplay.style.display = "none";
+      logButton.style.display = "block";
+      closeButton.style.display="none";
+    }
+  }
 }
