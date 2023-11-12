9f1b7b9cd5a0262f48a47a97a797e7eb1b12b46d
creleb <z1930375@students.niu.edu>
2023-11-11 22:17:01 -0600

    viewquotes lists all quotes and redirects

--- src/app/viewquotes/viewquotes.component.ts
+++ src/app/viewquotes/viewquotes.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd,ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-viewquotes',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./viewquotes.component.css']
 })
 export class ViewquotesComponent {
+
+  Quote: any[] = [];
+  constructor(private http: HttpClient, private router: Router) { }
+
+  ngOnInit() {
+    this.http.get('https://phpapicsci467.azurewebsites.net/php_script/selectQuote.php').subscribe((response: any) => {
+      this.Quote = response;
+      console.log(this.Quote);
+    });
+  }
+
+  ViewQuote() {
+    this.router.navigateByUrl('/quoteedit');
+  }
 
 }
