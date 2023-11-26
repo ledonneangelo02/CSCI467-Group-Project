c66073a08b915fd0ce2f81cc49936adff7e7f6f0
Angelo N. L. <ledonneangelo02@gmail.com>
2023-11-26 15:39:26 -0600

    small updates

--- src/app/pocreate/pocreate.component.ts
+++ src/app/pocreate/pocreate.component.ts
@@ -1,19 +1,7 @@
 import { Component, Inject, ElementRef, Renderer2, ViewChild, OnInit, createComponent } from '@angular/core';
 import { Router, NavigationEnd,ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import {
-  MatDialog,
-  MAT_DIALOG_DATA,
-  MatDialogRef,
-  MatDialogTitle,
-  MatDialogContent,
-  MatDialogActions,
-  MatDialogClose,
-} from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-pocreate',
