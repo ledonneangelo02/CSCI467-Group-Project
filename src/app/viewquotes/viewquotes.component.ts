2df6a7e7b04088765c66bcac5c49a5c7ff0a5634
creleb <z1930375@students.niu.edu>
2023-11-30 17:33:39 -0600

    added a simple search features

--- src/app/viewquotes/viewquotes.component.ts
+++ src/app/viewquotes/viewquotes.component.ts
@@ -9,18 +9,71 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ViewquotesComponent {
 
-  Quote: any[] = [];
+  Quotes: any[] = [];
+
+  SearchID: any;
+  SearchName: any;
+  SearchStatus: any;
+
+
   constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+    this.FillPage();
+  }
+
+  FillPage() {
+    this.GetQuotes();
+  }
+
+  GetQuotes() {
     this.http.get('https://phpapicsci467.azurewebsites.net/php_script/viewquote/selectQuote.php').subscribe((response: any) => {
-      this.Quote = response;
-      console.log(this.Quote);
+      this.Quotes = response;
     });
   }
 
   ViewQuote(id: number) {
     this.router.navigate(['/quoteedit'], {state: {data: id}});
+  }
+
+  isSearched(quote: any) {
+
+    let isSearched: boolean = true;
+
+    if (this.SearchID)
+    {
+      let IDString: string = quote['ID'];
+      if(!IDString.includes(this.SearchID))
+      {
+        isSearched = false;
+      }
+    }
+
+    if (this.SearchName)
+    {
+      let NameString: string = quote['CustName'];
+      let TempSearchName: string = this.SearchName;
+
+
+      if(!NameString.toLowerCase().includes(TempSearchName.toLowerCase()))
+      {
+        isSearched = false;
+      }
+    }
+
+    if (this.SearchStatus)
+    {
+      let StatusString: string = quote['Status'];
+      let TempStatusString: string = this.SearchStatus;
+
+
+      if (StatusString != TempStatusString.toUpperCase())
+      {
+        isSearched = false;
+      }
+    }
+
+    return isSearched;
   }
 
 }
