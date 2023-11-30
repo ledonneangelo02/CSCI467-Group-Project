2b3379a08da83ca4de934eafad384045f3da15f2
creleb <z1930375@students.niu.edu>
2023-11-30 17:47:09 -0600

    documentation

--- src/app/viewquotes/viewquotes.component.ts
+++ src/app/viewquotes/viewquotes.component.ts
@@ -9,64 +9,80 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ViewquotesComponent {
 
+  // Holds all quotes recieved from PHP.
   Quotes: any[] = [];
 
+  // Search terms.
   SearchID: any;
   SearchName: any;
   SearchStatus: any;
 
-
+  /***********************************************************
+  * Constructor.                                             *
+  ***********************************************************/
   constructor(private http: HttpClient, private router: Router) { }
 
+  /***********************************************************
+  * This function runs when the page initializes.            *
+  ***********************************************************/
   ngOnInit() {
     this.FillPage();
   }
 
+  /***********************************************************
+  * This function gets the quotes, which is filled in.       *
+  * the HTML                                                 *
+  ***********************************************************/
   FillPage() {
-    this.GetQuotes();
-  }
-
-  GetQuotes() {
     this.http.get('https://phpapicsci467.azurewebsites.net/php_script/viewquote/selectQuote.php').subscribe((response: any) => {
       this.Quotes = response;
     });
   }
 
-  ViewQuote(id: number) {
-    this.router.navigate(['/quoteedit'], {state: {data: id}});
-  }
-
+  /***********************************************************
+  * This function determines if a quote was searched. It is  *
+  * used in the HTML to display quotes if it returns true,   *
+  * and hides them if false.                                 *
+  ***********************************************************/
   isSearched(quote: any) {
-
+    // Declare boolean; will be returned.
     let isSearched: boolean = true;
 
+    // Check if SearchID is defined.
     if (this.SearchID)
     {
+      // Create a string of the quote's id.
       let IDString: string = quote['ID'];
+      // Check if the quote id doesn't contain the search id.
       if(!IDString.includes(this.SearchID))
       {
+        // Set isSearched to false.
         isSearched = false;
       }
     }
 
+    // Check if SearchName is defined.
     if (this.SearchName)
     {
+      // Declare a string of the quote customer and the search name.
       let NameString: string = quote['CustName'];
       let TempSearchName: string = this.SearchName;
 
-
+      // Check if quote's customer doesn't contain search name.
       if(!NameString.toLowerCase().includes(TempSearchName.toLowerCase()))
       {
+        // Set isSearched to false.
         isSearched = false;
       }
     }
 
+    // Similar to previous if statement.
     if (this.SearchStatus)
     {
       let StatusString: string = quote['Status'];
       let TempStatusString: string = this.SearchStatus;
 
-
+      // Check if the Status matches the temp status.
       if (StatusString != TempStatusString.toUpperCase())
       {
         isSearched = false;
@@ -76,4 +92,11 @@ export class ViewquotesComponent {
     return isSearched;
   }
 
+  /***********************************************************
+  * This function navigates to the quote edit screen with    *
+  * the selected id.                                         *
+  ***********************************************************/
+  ViewQuote(id: number) {
+    this.router.navigate(['/quoteedit'], {state: {data: id}});
+  }
 }
